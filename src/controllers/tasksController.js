98d8e880b4b34298bd005b0e5ea4ca34f7a1e0ef
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-03-07 16:09:51 -0300

    .

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,13 +1,21 @@
-const taks = require('../models/tasksModel')
+const tasks = require('../models/tasksModel.js')
 
 class TasksControllers{
 
     static async listartasks (req, res) {
         try {
-            const listartasks = await taks.find({})
+            const listartasks = await tasks.find({})
             res.status(200).json(listartasks)
         } catch (error) {
             res.status(501).json({  message: `${error.message} - falha na requisição ` })
+        }
+    }
+    static async crearTasks (req, res) {
+        try {
+            const novaTasks = await tasks.create(req.body)
+            res.status(201).json(novaTasks)
+        } catch (error) {
+            res.status(500).json({ message: `Falha ao criar --- ${error}` })
         }
     }
 
--- src/models/tasksModel.js
+++ src/models/tasksModel.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const taksSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     nome: { type: String, required: true },
-}, { versionKey: false })
+    status: { type: String, required: true },
+    created_date: { type: Date, default: Date.now  }
+}, { versionKey: false }) 
 
 const taks = mongoose.model('tasks', taksSchema)
 
